00d173547bf2bb56d8b887f5d5a405d0e320896b
Sudhanva Bharadwaj B M <kodandic@gmail.com>
2026-02-13 23:30:48 +0530

    harcoded-health-status

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	
+	"github.com/kodanda2301/orchestrix/pkg/api" // Import the shared types
 )
 
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	// In the future, we will read real CPU stats here.
+	// For now, we return a mock status.
+	status := api.HealthStatus{
+		Status:    "ready",
+		CPUUsage:  12.5,
+		RAMFree:   8192,
+		ActiveJob: false,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(status)
+}
+
 func main() {
-	fmt.Println("Worker node starting on port:- 8081")
-	http.HandleFunc("/infer", func(w http.ResponseWriter, r *http.Request) { // why * ? * means any method (GET, POST, etc.)
-		fmt.Fprintf(w, "Inference request received at Worker")
-	})
+	fmt.Println("🔧 Worker Node starting on port 8081...")
+
+	http.HandleFunc("/health", healthHandler) // <--- New endpoint
 
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		log.Fatal(err)
 	}
-
 }
